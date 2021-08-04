025a7d6325be27cc2d908e896156586f32591d31
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-08-04 23:28:34 +0530

    added fixed images and inline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Top from "./components/top/Top";
 import Mid from "./components/mid/Mid";
 import Bottom from "./components/bottom/Bottom";
+import "./styles/main.css";
 
 function App() {
   return (
-    <div>
-      <Top />
-      <Mid />
-      <Bottom />
+    <div className="bigAppDiv">
+      <div className="smallAppDiv">
+        <Top />
+        <Mid />
+        <Bottom />
+      </div>
     </div>
   );
 }

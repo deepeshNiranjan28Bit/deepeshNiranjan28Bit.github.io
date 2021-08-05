f96f4d94a4bace1766872e95bf1062baea5bcdd3
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-08-05 15:18:24 +0530

    added fonts

--- src/components/top/Top.js
+++ src/components/top/Top.js
@@ -5,8 +5,20 @@ function top() {
   return (
     <div className="topDiv">
       <div className="topInnerDiv">
-        <h1>Hello, I am</h1>
-        <h1>Deepesh</h1>
+        <h1
+          style={{
+            fontFamily: "'Libre Baskerville', serif",
+            marginBottom: "0",
+          }}
+        >
+          Hello, I am
+        </h1>
+        <br />
+        <h1
+          style={{ fontFamily: "'Libre Baskerville', serif", marginTop: "0" }}
+        >
+          Deepesh Niranjan
+        </h1>
         <p>
           The point of using Lorem Ipsum is that it has a more-or-less normal
           distribution of letters, as opposed to using 'Content here, content

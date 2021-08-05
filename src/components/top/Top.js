568dd7baf1fad30304140f47eddab47fba8d54c2
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-08-05 09:38:31 +0530

    added center

--- src/components/top/Top.js
+++ src/components/top/Top.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "../../styles/top.css";
 
 function top() {
-  return <div>Topz</div>;
+  return (
+    <div className="topDiv">
+      <div>
+        <h1>Hello, I am</h1>
+        <h1>Deepesh</h1>
+        <p>
+          t is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here',
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default top;
